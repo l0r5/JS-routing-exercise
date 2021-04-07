6de5264c078aa765ba6796a63ea4533fb7127ef6
l0r5 <lars-braitinger@web.de>
2021-04-07 21:26:46 +0200

    Use Switch to only load one route at a time

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -39,11 +39,11 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    <Link to={'/' + post.id} key={post.id}>
+                    <Link to={'/posts/' + post.id} key={post.id}>
                         <Post
                             title={post.title}
                             author={post.author}
-                            clicked={() => this.postSelectedHandler(post.id)}/>;
+                            clicked={() => this.postSelectedHandler(post.id)}/>
                     </Link>);
             });
         }
