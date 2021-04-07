069dae97c7bab7200ef917cf0a9ee8a591d742c3
l0r5 <lars-braitinger@web.de>
2021-04-07 22:32:09 +0200

    Implement lazy loading for NewPost

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
 // import axios from 'axios';
 import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
+
+
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
 
     render() {
@@ -33,7 +40,7 @@ class Blog extends Component {
                 </header>
                 <Switch>
                     // Order is important!
-                    { this.state.auth ? <Route path="/new-post" component={NewPost}/> : null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null}
                     <Route path="/posts" component={Posts}/>
                     <Route render={() => <h1>Not found</h1>}/>
                     {/*<Redirect  from="/" to="/posts" />*/}
