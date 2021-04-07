3e25fdb4590fbff2e442a32ed439e356597f152a
l0r5 <lars-braitinger@web.de>
2021-04-07 21:48:48 +0200

    Create nested routes for /posts/ with id

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,7 +4,6 @@ import {Route, NavLink, Switch} from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
 
@@ -15,7 +14,7 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><NavLink
-                                to="/"
+                                to="/posts/"
                                 activeClassName="my-active"
                                 activeStyle={{
                                     color: '#fa923f',
@@ -29,12 +28,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact component={Posts}/>
                 <Switch>
-                    {/*<Route path="/" exact render={() => <h1>Home</h1>}/>*/}
-                    {/*<Route path="/" render={() => <h1>Home 2</h1>}/>*/}
+                    // Order is important!
                     <Route path="/new-post" component={NewPost}/>
-                    <Route path="/:id" exact component={FullPost}/>
+                    <Route path="/posts" component={Posts}/>
                 </Switch>
             </div>
         );
